ad3f92da89f20c646a2c5f3a33106f819c9a70cd
fabiodevelopment <fabiohpaes@gmail.com>
2022-09-12 18:09:47 -0300

    add prop requestra fatura

--- checkout-ui-custom/checkout6-custom.js
+++ checkout-ui-custom/checkout6-custom.js
@@ -1,15 +1,15 @@
 // This is an example
 function setVtexAppsConfig(vtexAppsConfig, appName) {
-    if (appName == "checkout-v6-invoice-data") {
-      vtexAppsConfig.locale = "it";
-      vtexAppsConfig.invoiceDataMandatory = true;
-      vtexAppsConfig.showSDIPECSelector = true;
-      vtexAppsConfig.showPersonTypeSelector = true;
-      vtexAppsConfig.defaultSDIPEC="sdi"
-      vtexAppsConfig.defaultPersonType = "company";
-      vtexAppsConfig.requiredFields.it = {
-        profile: ["user-person-type", "custom-corporate-name"],
-        address: ["custom-corporate-street"],
-      };
-    }
+  if (appName == "checkout-v6-invoice-data") {
+    vtexAppsConfig.locale = "it";
+    vtexAppsConfig.invoiceDataMandatory = false;
+    vtexAppsConfig.showSDIPECSelector = true;
+    vtexAppsConfig.showPersonTypeSelector = true;
+    vtexAppsConfig.defaultSDIPEC="sdi"
+    vtexAppsConfig.defaultPersonType = "company";
+    vtexAppsConfig.requiredFields.it = {
+      profile: ["user-person-type", "custom-corporate-name"],
+      address: ["custom-corporate-street"],
+    };
   }
+}
